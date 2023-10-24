fa395d7a1fbbe43bde789b49fba6c7bab5de3efb
praprf <fil18mot@gmail.com>
2023-10-24 13:54:16 +0300

    finished character features

--- TowerDefenseStrelyalkaCarti/Assets/_Source/CharacterSystem/Character.cs
+++ TowerDefenseStrelyalkaCarti/Assets/_Source/CharacterSystem/Character.cs
@@ -5,6 +5,7 @@ namespace CharacterSystem
     [RequireComponent(typeof(Rigidbody2D))]
     public class Character : MonoBehaviour
     {
+        [field: SerializeField] public float Health { get; private set; }
         [field: SerializeField] public float MovementSpeed { get; private set; }
         [field: SerializeField] public Rigidbody2D Rb { get; private set; }
         [field: SerializeField] public GameObject BulletPrefab { get; private set; }
