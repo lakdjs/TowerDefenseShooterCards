0e49c24fc8f97e39c4d2c519a61b04f543a5a23b
praprf <fil18mot@gmail.com>
2023-10-24 15:03:20 +0300

    started creating enemy

---
+++ TowerDefenseStrelyalkaCarti/Assets/_Source/EnemySystem/Enemy.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace EnemySystem
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Enemy : MonoBehaviour
+    {
+        [field: SerializeField] public float Health { get; private set; }
+        [field: SerializeField] public float MovementSpeed { get; private set; }
+        [field: SerializeField] public Rigidbody2D Rb { get; private set; }
+        [field: SerializeField] public GameObject BulletPrefab { get; private set; }
+        [field: SerializeField] public Transform FirePoint { get; private set; }
+    }
+}
