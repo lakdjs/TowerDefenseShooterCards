cc49e89c992ca52bf3d97d771702d5ab5156b61a
kousaatsu <121098273+kousaatsu@users.noreply.github.com>
2023-10-25 17:36:38 +0300

    Fix: Fixed cards moving to deck

--- TowerDefenseStrelyalkaCarti/Assets/_Source/CardSystem/ShopSystem.cs
+++ TowerDefenseStrelyalkaCarti/Assets/_Source/CardSystem/ShopSystem.cs
@@ -13,7 +13,7 @@ public class ShopSystem : MonoBehaviour
     public GameObject[] cardPanelsGO;
     public Button[] purchaseButton;
 
-    public Transform deck;
+    public GameObject deck;
     // Start is called before the first frame update
     void Start()
     {
