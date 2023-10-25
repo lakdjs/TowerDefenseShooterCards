9060bcb47dbaa93e56f6dbd9104771e30932a396
kousaatsu <121098273+kousaatsu@users.noreply.github.com>
2023-10-25 17:06:03 +0300

    Feat: Items are now purchasable

--- TowerDefenseStrelyalkaCarti/Assets/_Source/CardSystem/ShopSystem.cs
+++ TowerDefenseStrelyalkaCarti/Assets/_Source/CardSystem/ShopSystem.cs
@@ -10,11 +10,18 @@ public class ShopSystem : MonoBehaviour
     public TMP_Text coinsTxt;
     public SO[] SO;
     public CardTemplate[] cardPanels;
+    public GameObject[] cardPanelsGO;
+    public Button[] purchaseButton;
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < SO.Length; i++)
+        {
+            cardPanelsGO[i].SetActive(true);
+        }
         coinsTxt.text = "Coins: " + coins.ToString();
         LoadPanels();
+        canPurchase();
     }
 
     // Update is called once per frame
@@ -25,8 +32,9 @@ public class ShopSystem : MonoBehaviour
 
     public void AddCoins()
     {
-        coins = coins + 100;
+        coins += 100;
         coinsTxt.text = coinsTxt.text = "Coins: " + coins.ToString();
+        canPurchase();
     }
 
     public void LoadPanels()
@@ -36,6 +44,31 @@ public class ShopSystem : MonoBehaviour
             cardPanels[i].titleText.text = SO[i].title;
             cardPanels[i].descText.text = SO[i].description;
             cardPanels[i].costText.text = SO[i].cost.ToString();
+        }
+    }
+
+    public void canPurchase()
+    {
+        for (int i = 0; i < SO.Length; i++)
+        {
+            if (coins >= SO[i].cost)
+            {
+                purchaseButton[i].interactable = true;
+            }
+            else
+            {
+                purchaseButton[i].interactable = false;
+            }
+        }
+    }
+
+    public void PurchaseItem(int btnNo)
+    {
+        if (coins >= SO[btnNo].cost)
+        {
+            coins = coins - SO[btnNo].cost;
+            coinsTxt.text = "Coins: " + coins.ToString();
+            canPurchase();
         }
     }
 }
