af67cbd6c6df9727e3d44db23e41e2a292657457
kousaatsu <GrigoriantsSM21@st.ithub.ru>
2023-11-30 02:02:12 +0300

    Feat: Gates on E

--- TowerDefenseStrelyalkaCarti/Assets/_Source/Gates/GateFindCollider.cs
+++ TowerDefenseStrelyalkaCarti/Assets/_Source/Gates/GateFindCollider.cs
@@ -2,25 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GateFindCollider : MonoBehaviour
+namespace GatesSystem
 {
-    public GameObject gate;
-
-    private bool isOpened = true;
-    // Start is called before the first frame update
-
-
-    private void OnTriggerEnter2D(Collider2D other)
+    public class GateFindCollider : MonoBehaviour
     {
-        if (other.CompareTag("Character") && isOpened == true)
+        public GameObject gate;
+
+        private bool isOpened = true;
+
+        private bool gateState = true;
+        // Start is called before the first frame update
+
+
+        IEnumerator OnTriggerStay2D(Collider2D other)
         {
-            gate.SetActive(false);
-            isOpened = false;
-        }
-        else if (other.CompareTag("Character") && isOpened == false)
-        {
-            gate.SetActive(true);
-            isOpened = true;
+            if (other.CompareTag("Character") && isOpened == true && gateState == true)
+            {
+                if (Input.GetKey(KeyCode.E))
+                {
+                    gate.SetActive(false);
+                    isOpened = false;
+                    gateState = false;
+                    yield return new WaitForSeconds(0.3f);
+                    gateState = true;
+                }
+            }
+            else if (other.CompareTag("Character") && isOpened == false && gateState == true)
+            {
+                if (Input.GetKey(KeyCode.E))
+                {
+                    gate.SetActive(true);
+                    isOpened = true;
+                    gateState = false;
+                    yield return new WaitForSeconds(0.3f);
+                    gateState = true;
+                }
+            }
         }
     }
 }
